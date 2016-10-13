7211c49a2a9eff14d35f06bc7d690ef0e87804d7
fangpeiqing <729882885@qq.com>
2016-10-13 17:34:58 +0800

    添加es6配置

--- src/app/app.spec.js
+++ src/app/app.spec.js
@@ -14,7 +14,7 @@ describe('app', () => {
     });
 
     it('should contain the starter url', () => {
-      expect(ctrl.url).toBe('https://github.com/preboot/angular-webpack');
+      expect(ctrl.url).toBe('ttps://github.com/zuiyuexuan/webpack-es6-angular1.x');
     });
   });
 });
