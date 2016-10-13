a5b6ce6af7f1c14c36343ba04d5dc9ed2d720e79
fangpeiqing <729882885@qq.com>
2016-10-13 18:02:56 +0800

    调整表格间距

--- src/features/manageCenter/domainManager/domain.edit.controller.js
+++ src/features/manageCenter/domainManager/domain.edit.controller.js
@@ -7,11 +7,8 @@ export default class DomainEditController {
     $onInit() {
         const parentScope = this.parentScope;
         const title = "域名";
-
         this.formType = parentScope.formType;
         this.title = this.formType == 'edit' ? '修改'+title : '新增'+title
-
-
     }
 
 
